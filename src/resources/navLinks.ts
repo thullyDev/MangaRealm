9a556cd293d25e9754028219809fb736e0034b2e
thullyDev <thully.developer@gmail.com>
2024-04-21 06:45:29 +0200

    finished the nav_links

--- src/resources/navLinks.ts
+++ src/resources/navLinks.ts
@@ -8,4 +8,28 @@ export const navs: NavItem[] = [
     name: "Popular",
     link: "/popular",
   },
+  {
+    name: "Recent",
+    link: "/recent",
+  },
+  {
+    name: "Complete",
+    link: "/complete",
+  },
+  {
+    name: "Ongoings",
+    link: "/ongoings",
+  },
+  {
+    name: "Top",
+    link: "/top",
+  },
+  {
+    name: "Newest",
+    link: "/newest",
+  },
+  {
+    name: "Genres",
+    link: "#genres",
+  },
 ];
