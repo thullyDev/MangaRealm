778471218f551c7ddcd7eb9d22dd01a28f89f261
thullyDev <thully.developer@gmail.com>
2024-05-20 19:15:37 +0200

    add more link to mangas wrapper

--- src/components/Widgets/MangasWrapper/MangasWrapper.tsx
+++ src/components/Widgets/MangasWrapper/MangasWrapper.tsx
@@ -1,22 +1,38 @@
 import type { Manga } from "../../../services/Manganato/manganatoTypes"
 import { MangaWrapperOne } from "../MangaWrapperOne/MangaWrapperOne"
+import { MangaWrapperThree } from "../MangaWrapperThree/MangaWrapperThree"
 import { MangaWrapperTwo } from "../MangaWrapperTwo/MangaWrapperTwo"
 
-interface MangasWrapperProps { data: Manga[], className: string, label: string, wrapper: number }
+interface _MangasWrapper {
+	data: Manga[], 
+	className: string, 
+	label: string, 
+	wrapper: number,
+	moreLink: string, 
+}
 
-export const MangasWrapper = ({ data, className, label, wrapper = 1 }: MangasWrapperProps) => {
+export const MangasWrapper = ({ data, className, label, moreLink, wrapper = 1 }: _MangasWrapper) => {
 	return (
 		<div className={"mangas-con " + className}>
-			<div className="manga-label-con">
+			<div className="manga-label-more-con">
 				<h3 className="manga-label">{label}</h3>
+				<span className="more-wrapper">
+					{
+						moreLink && <a href={moreLink} className="more-link">more</a>
+					}
+				</span>
 			</div>
 			<div className="inner-con">
 				{data.map((item, index) => {
-						return ( 
-							wrapper == 1 ? 
-								<MangaWrapperOne key={index} item={item}/> : 
-								<MangaWrapperTwo key={index} item={item}/>
-						)
+						if (wrapper == 1) {
+							return <MangaWrapperOne key={index} item={item}/>
+						}
+
+						if (wrapper == 2) {
+							return <MangaWrapperTwo key={index} item={item}/>
+						}
+
+						return <MangaWrapperThree key={index} item={item}/>
 					})}
 			</div>
 		</div>
