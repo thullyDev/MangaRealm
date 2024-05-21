4ded382434d5a68d12aaa00b9aecc77c1e473890
thullyDev <thully.developer@gmail.com>
2024-05-21 20:57:05 +0200

    checkpointing

---
+++ src/components/Widgets/InnerFeaturesSlider/innerFeaturesSlider.tsx
@@ -0,0 +1,13 @@
+import type { Manga } from "../../../services/Manganato/manganatoTypes";
+import { FeatureItem } from "../FeatureItem/FeatureItem";
+import { Slider } from "../Slider/Slider";
+import "./FeaturesSliderStyle.scss"
+
+
+export const FeaturesSlider = ({ data: className }: { data: Manga[]; className: string }) => {
+	return (
+		<div className="features-slider">
+			<Slider items={data.map((manga: Manga) => <FeatureItem item={manga} />)} className={className} />
+		</div>
+	)
+}
