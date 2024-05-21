fb04442aeee563b7439a61adeda30f5f29beb65b
thullyDev <thully.developer@gmail.com>
2024-05-22 01:18:56 +0200

    finished styling features slider

--- src/components/Widgets/FeatureItem/FeatureItem.tsx
+++ src/components/Widgets/FeatureItem/FeatureItem.tsx
@@ -1,49 +1,65 @@
 import type { Manga } from "../../../services/Manganato/manganatoTypes"
 
-export const FeatureItem = ({ item }: { item: Manga }) => {
+export const FeatureItem = ({ item, className }: { item: Manga, className: string }) => {
 	const { 
 			title, 
 			image_url, 
 			slug, 
 			status, 
 			synopsis, 
-			author, 
+			chapter, 
 			views, 
 			update, 
 			type, 
 			score 
 	} = item
+	const { name, slug: chapSlug } = chapter
+	const ticks = [ type, update, views, score ]
+	const tickEles = []
+
+	for (let i = 0; i < ticks.length; i++) {
+		const tick = ticks[i]
+		const item = ( 
+			<>	
+				<span className="tick dot w-1 h-1 bg-gray-500 rounded-full"></span>
+				<span className="tick text-xs text-gray-500">{tick}</span>
+			</>
+		)
+		tickEles.push(item)
+	}
+
 	return (
-			<div className="feature-item my-1" title={title}>
-				<div className="inner-con bg-gray-800 px-2 py-2 mx-2 rounded flex">
-					<div className="left-side-con">
-						<div className="inner-con">
+			<div className={`feature-item my-1 ${className}`} title={title}>
+				<div className="inner-con bg-zinc-800 border border-zinc-600 mx-2 rounded flex justify-between">
+					<div className="left-side-con pl-6 py-5">
+						<div className="inner-con flex flex-col gap-5 relative">
 							<div className="status text-xs text-gray-500">
 								{status}
 							</div>
 							<div className="title">
 								{title}
-							</div>	
+							</div>
+							<div className="chapter">
+								<a href={`/read/${slug}/${chapSlug}`} className="chapter-link text-xs text-gray-400 hover:text-red-400 transition duration-1000 ease-in-out">
+									{name}
+								</a>
+							</div>		
 							<div className="synopsis">
 								{synopsis}
 							</div>	
-							<div className="ticks">
+							<div className="ticks flex gap-1 items-center relative top-3">
 								{
-									[ type, author, update, views, score ].map((item, index) => {
-										return (
-											<span key={index} className="tick text-xs text-gray-00">{item}</span>
-										)
-									})
+									tickEles.map((tick) => tick)
 								}
 							</div>	
-							<div className="read-link-con">
-								<a href={slug} className="read-link">Read Now</a>
+							<div className="read-link-con flex justify-center">
+								<a href={`/read/${slug}`} className="read-link text-sm transition bg-red-700 hover:bg-red-500 mt-2 px-2 py-1 rounded-xl ease-in-out duration-1000">Read Now</a>
 							</div>								
 						</div>
 					</div>
-					<div className="right-side-con">
-						<div className="inner-con h-40">
-							<img src={image_url} alt={title} className="feature-image h-full object-cover"/>
+					<div className="right-side-con h-full">
+						<div className="inner-con h-full rounded-r overflow-hidden">
+							<img src={image_url} alt={title} className="feature-image relative left-5 bottom-2 rotate-12 h-64 w-36"/>
 						</div>
 					</div>
 				</div>
--- src/components/Widgets/FeaturesSlider/FeaturesSlider.tsx
+++ src/components/Widgets/FeaturesSlider/FeaturesSlider.tsx
@@ -1,13 +1,20 @@
 import type { Manga } from "../../../services/Manganato/manganatoTypes";
 import { FeatureItem } from "../FeatureItem/FeatureItem";
 import { Slider } from "../Slider/Slider";
-// import "./FeaturesSliderStyle.scss"
+import "./FeaturesSliderStyle.scss"
 
 
 export const FeaturesSlider = ({ data, className }: { data: Manga[]; className: string }) => {
+	const slides = []
+	for (let i = 0; i < data.length; i++) {
+		const className = i == 1 ? "active" : ""
+		const manga = data[i]
+		const slide = <FeatureItem className={className} item={manga} />
+		slides.push(slide)
+	}
 	return (
 		<div className="features-slider">
-			<Slider items={data.map((manga: Manga) => <FeatureItem item={manga} />)} className={className} />
+			<Slider items={slides} className={className} />
 		</div>
 	)
 }
