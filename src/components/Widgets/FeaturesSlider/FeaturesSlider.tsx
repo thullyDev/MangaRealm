e150dc78b7f306eea0675c357ebaef0dc270f2e5
thullyDev <thully.developer@gmail.com>
2024-05-21 21:21:35 +0200

    reverted back to older commit

--- src/components/Widgets/FeaturesSlider/FeaturesSlider.tsx
+++ src/components/Widgets/FeaturesSlider/FeaturesSlider.tsx
@@ -1,10 +1,10 @@
 import type { Manga } from "../../../services/Manganato/manganatoTypes";
 import { FeatureItem } from "../FeatureItem/FeatureItem";
 import { Slider } from "../Slider/Slider";
-import "./FeaturesSliderStyle.scss"
+// import "./FeaturesSliderStyle.scss"
 
 
-export const FeaturesSlider = ({ data: className }: { data: Manga[]; className: string }) => {
+export const FeaturesSlider = ({ data, className }: { data: Manga[]; className: string }) => {
 	return (
 		<div className="features-slider">
 			<Slider items={data.map((manga: Manga) => <FeatureItem item={manga} />)} className={className} />
