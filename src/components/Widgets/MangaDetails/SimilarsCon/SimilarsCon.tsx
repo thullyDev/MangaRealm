986ae9ee1a254770b8b7d75ab39abbf1a5b680a9
thullyDev <thully.developer@gmail.com>
2024-05-26 00:15:33 +0200

    did the styling for the similars mangas

--- src/components/Widgets/MangaDetails/SimilarsCon/SimilarsCon.tsx
+++ src/components/Widgets/MangaDetails/SimilarsCon/SimilarsCon.tsx
@@ -3,18 +3,18 @@ import { MangaWrapperThree } from "../../MangaWrapperThree/MangaWrapperThree"
 
 export const SimilarsCon = ({ similars }: { similars: Manga[] }) => {
 	return (
-		<div className="similars-con">
+		<div className="similars-con mt-10">
 			<div className="label-con">
-				<h4 className="similars-label">
+				<h4 className="similars-label flex justify-center">
 					You may also like
 				</h4>
 			</div>
 			<div className="inner-similars-con">
-				<ul className="similars-list">
+				<ul className="similars-list mt-2 flex flex-col gap-2 items-center">
 					{
 						similars.map((manga, index) => {
 							return (
-								<li key={index} className="similar-item">
+								<li key={index} className="similar-item w-full max-w-96">
 									<MangaWrapperThree item={manga}/>
 								</li>
 							)
