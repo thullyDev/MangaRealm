9b5e7397520869b8b80a66dfb67972a42cbeebbe
thullyDev <thully.developer@gmail.com>
2024-05-27 01:54:17 +0200

    did the animating for opening the menu

---
+++ src/components/global/MobileMenu/MenuBtn.tsx
@@ -0,0 +1,14 @@
+import { showCloseEle } from "../../../utilities/misc";
+
+export const MenuBtn = () => {
+	return (
+		<button
+			onClick={showCloseEle}
+			data-element="nav.menu-nav"
+			data-animate="slide"
+			className="menu-button"
+		>
+			<i className="fas fa-bars text-xl"></i>
+		</button>
+	);
+};
--- src/utilities/misc.ts
+++ src/utilities/misc.ts
@@ -1,3 +1,5 @@
+import $ from "jquery";
+
 export const trans1000 = "transition duration-1000 ease-in-out"
 export const trans500 = "transition duration-500 ease-in-out"
 export const truncate = (input: string, length: number) => {
@@ -8,3 +10,21 @@ export const truncate = (input: string, length: number) => {
 };
 
 export const formatKey = (key: string) => key.replace(/_/g, ' ').replace(/\b\w/g, (char) => char.toUpperCase());
+
+export const showCloseEle = (event: React.MouseEvent<HTMLButtonElement>) => {
+   const eventEle = $(event.currentTarget);
+   const element = eventEle.data("element");
+   const animate = eventEle.data("animate");
+   const showEle = $(element);
+   const open = showEle.data("open");
+
+   if (animate == "fade") {
+      !open
+         ? showEle.fadeIn().data("open", true)
+         : showEle.fadeOut().data("open", false);
+      return;
+   }
+   !open
+      ? showEle.slideDown().data("open", true)
+      : showEle.slideUp().data("open", false);
+}
