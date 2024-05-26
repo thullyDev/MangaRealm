af4bcfa5df6fd22890933f3668781cc9757231d8
thullyDev <thully.developer@gmail.com>
2024-05-26 18:39:00 +0200

    did the styling to top

--- src/components/Widgets/Panels/Panels.tsx
+++ src/components/Widgets/Panels/Panels.tsx
@@ -1,10 +1,8 @@
 import type { Panel as PanelProp } from "../../../services/Manganato/manganatoTypes";
+import { trans500 } from "../../../utilities/misc";
 import { Panel } from "../Panel/Panel";
 
 export const Panels = ({ panels }: { panels: PanelProp[] }) => {
-	return (
-		<></>
-	)
 	return (
 		<div className="manga-panels-con">
 			<ul className="manga-panels-list">
@@ -21,9 +19,9 @@ export const Panels = ({ panels }: { panels: PanelProp[] }) => {
 					);
 				})}
 			</ul>
-			<button className="top-btn">
+			<a href="#" className={ `top-btn sticky bottom-5 left-5 p-3 bg-zinc-800 hover:bg-zinc-600 ${trans500}` }>
 				<i className="fa-solid fa-arrow-up"></i>
-			</button>
+			</a>
 		</div>
 	);
 };
