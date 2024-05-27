2e34b414a52b9ac6656aa7c441e61f5b36b2f693
thullyDev <thully.developer@gmail.com>
2024-05-28 01:51:34 +0200

    did some quick bug fixes

--- src/components/Widgets/MangaWrapperThree/MangaWrapperThree.tsx
+++ src/components/Widgets/MangaWrapperThree/MangaWrapperThree.tsx
@@ -13,7 +13,7 @@ export const MangaWrapperThree = ({ item }: { item: Manga }) => {
 	return (
 		<div className="manga-item w-full">
 			<div className="inner-con bg-zinc-800 border border-zinc-600 rounded-md">
-				<a href={slug} className="manga-link flex gap-2">
+				<a href={`/read${slug}`} className="manga-link flex gap-2">
 					<span className="poster-con">
 						<img src={image_url} alt={title} title={title} className="poster w-20 rounded-l-md"/>
 					</span>
