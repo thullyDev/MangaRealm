ecb55e517c566ad5ac77d342166d0abf7c91ad7f
thullyDev <thully.developer@gmail.com>
2024-06-08 13:06:21 +0200

    styled the captcha-widgets con

--- src/components/Widgets/HCaptcha/HCaptcha.tsx
+++ src/components/Widgets/HCaptcha/HCaptcha.tsx
@@ -1,7 +1,7 @@
 export const HCaptcha = ({ captchaId }: { captchaId: string }) => {
 	return (
 		<div className="captcha-con">
-			<div id={`captcha-${captchaId}`} className="captcha-widgets"></div>
+			<div id={`captcha-${captchaId}`} className="captcha-widgets flex justify-center mt-5"></div>
 		</div>
 	)
 }
